0e8b83310bb9e048f33aa7c7401c7d0b6904e9c3
Klaudia Kieryk <klaudiakieryk123@gmail.com>
2021-08-23 19:55:16 +0200

    Adding Button, Tasks and Task components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import {useState} from 'react'
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'First task',
+        day: 'Feb 5th at 2:30pm',
+        remainder: true
+    },
+    {
+        id: 2,
+        text: 'Second tast',
+        day: 'Feb 6th at 1:30pm',
+        remainder: false
+    }
+  ])
 
   return (
     <div className='container'>
       <Header />
+      <Tasks tasks={tasks}/>
    </div>
   );
 }
